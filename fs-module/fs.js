b51d534beb99fe5abf56d37ff00d18f36b3d20f3
vanshrajput01 <rajputvansh009@gmail.com>
2024-01-15 16:00:35 +0530

    Read file asynchronous using fs module in node.js

--- fs-module/fs.js
+++ fs-module/fs.js
@@ -1,5 +1,22 @@
 // fs Module 
 
 const fs = require("fs")
-console.log(fs);
+// console.log(fs);
+
+// Read File Asynchronous in fs
+
+fs.readFile("input.txt",(e,data)=>{
+    if(e){
+        console.log("Error is found : -",e);
+    }
+    console.log(data); // that is return Buffer data
+    console.log(data.toString());
+})
+
+
+// Read File synchronous in fs
+
+
+
+
 
